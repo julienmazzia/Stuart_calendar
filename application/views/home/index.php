25fb518c030e28669244fd95f9002adc4b59409f
Unknown <julienmazzia@icloud.com>
2018-01-23 10:31:35 +0100

    V0.8.1
    Change application aspects

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,28 +1,37 @@
 <div class="parallax-container" id="par1">
-      <div class="parallax"><img src="../<?php base_url(); ?>assets/images/boat.jpg"></div>
+	<h2 style="margin-top: 32vh;color: white;">I'm welcome to Stuart, but what is it exactly ?</h2>
+      <div class="parallax"><img src="../<?php base_url(); ?>assets/images/background6.jpg"></div>
     </div>
-
-    <h3>I'm welcome to Stuart, but what is it exactly ?</h3>
 
     <div class="row">
 	    	<img class="col s5" src="../<?php base_url(); ?>assets/images/home.png">
 
-		<div class="col s6">
+		<div class="col s7">
 		    <h4>Stuart is more than a calendar, it is the calendar !</h4>
-		    <p>I had, for a long time, used different calendars in my life. But I always had many problems "It's too long", "I can't really see what appointment I have in my day !", "Gasp !"</br> Then one day, I told myself : " Why didn't I create my own calendar ? " </br> Then this project came thrue ! </br></br> But in fact, it's cool to have my calendar but it's better if anyone can use and enjoy it ?</p>
+		    <h6>I had, for a long time, used different calendars in my life. But I always had many problems "It's too long", "I can't really see what appointment I have in my day !", "Gasp !"</br> Then one day, I told myself : " Why didn't I create my own calendar ? " </br> Then this project came thrue ! </br></br> But in fact, it's cool to have my calendar but it's better if anyone can use and enjoy it ?</h6>
 		</div>
     </div>
 
     <div class="row">
-    	<div class="col s5">
-    	 <p> With Stuart you can add any type of appointment at any time, any where. By just little click</p>
+    	<div class="col s4">
+    		<h5>Organize your life !</h5>
     	</div>
-    	 <img class="col s6" src="../<?php base_url(); ?>assets/images/add.png">
+    	<!--<div class="col s6 offset-s1 multiple-borders">
+    		<div class="row">
+    			<img class="col s6 effect" src="../<?php base_url(); ?>assets/images/dinner.jpeg">
+    			<img class="col s6 effect" src="../<?php base_url(); ?>assets/images/party.jpeg">
+    		</div>
+    		<div>
+    			<img class="col s6 effect" src="../<?php base_url(); ?>assets/images/meeting.jpeg">
+    			<img class="col s6 effect" src="../<?php base_url(); ?>assets/images/concert.jpeg">
+    		</div>
+    	</div>-->
     </div>
 
 
     <div class="parallax-container" id="par2">
-      <div class="parallax"><img src="../<?php base_url(); ?>assets/images/background.jpeg"></div>
+    		<h4 class="parallax-title">With Stuart you can add any type of appointment at any time, any where. </br>By just some click !</h4>
+      <div class="parallax"><img src="../<?php base_url(); ?>assets/images/add.png"></div>
     </div>
 
     
