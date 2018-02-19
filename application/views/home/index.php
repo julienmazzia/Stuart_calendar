232a3d4c4ab9f8cecda61cc0cd97c2e381b9f297
Unknown <julienmazzia@icloud.com>
2018-02-19 10:45:42 +0100

    V1.3.1
    One page home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -109,6 +109,50 @@
 
     </div>
 
+    <div class="border dark-1 shadow-top"></div>
+
+<div id="contact">
+  <div class="container">
+    <h1>
+      Contact us !
+    </h1>
+
+    <div class="row" style="margin-top: 15vh;">
+      <form class="col s12">
+
+        <div class="row">
+          <div class="input-field col l3 s6">
+            <i class="material-icons prefix up">account_circle</i>
+            <input id="icon_prefix" placeholder="John" type="text" class="validate browser-default">
+            <label for="icon_prefix">Name</label>
+          </div>
+
+          <div class="input-field col l3 s6" style="margin-bottom: 10px;">
+            <i class="material-icons prefix up">mail</i>
+            <input id="icon_telephone" placeholder="example@ex.com" type="email" class="validate browser-default">
+            <label for="icon_telephone" data-error="example@ex.com">Mail</label>
+          </div>
+
+          <div class="input-field col s6">
+            <i class="material-icons prefix up"></i>
+            <input id="icon_telephone" placeholder="Day view" type="text" class="validate browser-default">
+            <label for="icon_telephone" data-error="example@ex.com">Subject</label>
+          </div>
+
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea"></textarea>
+          </div>
+
+          <button style="margin-left: 1.25vh;" class="btn waves-effect waves-light col" type="submit" name="action">Submit
+          <i class="material-icons right">send</i>
+          </button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+
 
 
 
@@ -124,7 +168,7 @@
 
       <li><a class="btn-floating hover-float tooltipped green" data-position="top" data-delay="50" data-tooltip="Join us !" href="../Account/createAccount"><i class="material-icons">sentiment_very_satisfied</i></a></li>
 
-      <li><a class="btn-floating hover-float tooltipped blue" data-position="top" data-delay="50" data-tooltip="contact us !" href="../Home/contact"><i class="material-icons">email</i></a></li>
+      <li><a class="btn-floating hover-float tooltipped blue" data-position="top" data-delay="50" data-tooltip="contact us !" href="#contact"><i class="material-icons">email</i></a></li>
 
     </ul>
   </div>
