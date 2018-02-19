314ec747c2902c82981ec22e3a1b40e15c833915
Unknown <julienmazzia@icloud.com>
2018-02-19 09:26:14 +0100

    V1.2.3
    Contact page improvements

--- application/views/contact/form.php
+++ application/views/contact/form.php
@@ -5,24 +5,35 @@
 
 	<div class="row" style="margin-top: 15vh;">
     <form class="col s12">
+
       <div class="row">
-        <div class="input-field col s3">
-          <i class="material-icons prefix">account_circle</i>
+        <div class="input-field col l3 s6">
+          <i class="material-icons prefix up">account_circle</i>
           <input id="icon_prefix" placeholder="John" type="text" class="validate browser-default">
           <label for="icon_prefix">Name</label>
         </div>
-        <div class="input-field col s3">
-          <i class="material-icons prefix">mail</i>
-          <input id="icon_telephone" placeholder="example@ex.com" type="tel" class="validate browser-default">
+
+        <div class="input-field col l3 s6" style="margin-bottom: 10px;">
+          <i class="material-icons prefix up">mail</i>
+          <input id="icon_telephone" placeholder="example@ex.com" type="email" class="validate browser-default">
           <label for="icon_telephone" data-error="example@ex.com">Mail</label>
         </div>
+
+        <div class="input-field col s6">
+          <i class="material-icons prefix up"></i>
+          <input id="icon_telephone" placeholder="Day view" type="text" class="validate browser-default">
+          <label for="icon_telephone" data-error="example@ex.com">Subject</label>
+        </div>
+
         <div class="input-field col s12">
           <textarea id="textarea1" class="materialize-textarea"></textarea>
         </div>
+
         <button style="margin-left: 1.25vh;" class="btn waves-effect waves-light col" type="submit" name="action">Submit
   	    <i class="material-icons right">send</i>
-  	  </button>
+  	  	</button>
       </div>
+
     </form>
   </div>
 </div>
